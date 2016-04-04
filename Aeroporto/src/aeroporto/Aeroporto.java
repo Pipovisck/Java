5779370dd4408124a49f5d6a55f17e4072c51722
Gabriel Haddad <gabriel.haddad15@gmail.com>
2016-04-04 10:50:00 -0300

    Aprovado por Zé Wilson

--- Aeroporto/src/aeroporto/Aeroporto.java
+++ Aeroporto/src/aeroporto/Aeroporto.java
@@ -3,7 +3,7 @@ package aeroporto;
 import javax.swing.JOptionPane;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
-
+import javax.swing.JPanel;
 /**
  *
  * @author GABRIEL HADDAD
@@ -15,28 +15,31 @@ public class Aeroporto {
      */
     public static void main(String[] args) {
         int nVoos=6; 
+        
         passageiro p = new passageiro();
         voo voo[]= new voo[nVoos];
         JTextArea CaixaSaida = new JTextArea(11,20);
         JFrame frame=new JFrame();
+        JPanel panel= new JPanel();
+        
         CaixaSaida.append("Identidade\tNº do Voo\tPreço\tSituaçao\n");
         for(int i=0;i<nVoos;i++){  //gera os 5 vôos 
             voo[i]=new voo();
-            voo[i].setnVoo(Integer.parseInt(JOptionPane.showInputDialog(null,"Insira nº do voo: ")));
-            voo[i].settAviao(Integer.parseInt(JOptionPane.showInputDialog(null,"Insira tipo do aviao: ")));
-            voo[i].setPreco(Integer.parseInt(JOptionPane.showInputDialog(null,"Insira preço do voo: ")));
+            voo[i].setnVoo(Integer.parseInt(JOptionPane.showInputDialog(null,"Insira nº do voo: ","Vôo["+i+"]",JOptionPane.QUESTION_MESSAGE)));
+            voo[i].settAviao(Integer.parseInt(JOptionPane.showInputDialog(null,"Insira tipo do aviao: ","Vôo["+i+"]",JOptionPane.QUESTION_MESSAGE)));
+            voo[i].setPreco(Integer.parseInt(JOptionPane.showInputDialog(null,"Insira preço do voo: ","Vôo["+i+"]",JOptionPane.QUESTION_MESSAGE)));
         }
         p.setId(Integer.parseInt(JOptionPane.showInputDialog(null,"Insira Id do passageiro: ")));
         while(p.getId()!=0){
             p.setVoo(Integer.parseInt(JOptionPane.showInputDialog(null,"Insira o voo desejado: ")));
-            for (int j=0;j<nVoos;j++){
-                if(voo[j].getnVoo()==p.getVoo()){
-                    if(voo[j].CheckLugar()){
+            for (int j=0;j<nVoos;j++){ //le os vôos 
+                if(voo[j].getnVoo()==p.getVoo()){  //compara o nº do vôo com o vôo desejado
+                    if(voo[j].CheckLugar()){  //verifica a disponibilidade de lugar ( Boolean ) 
                         voo[j].setPassageiro(p); //envia os dados para classe Voo e dps classe passageiro
                        /* JOptionPane.showMessageDialog(null,"Id: "+p.getId()+
                                                     "\nNº do Voo: "+p.getVoo()+
                                                     "\nPreço: "+ voo[j].getPreco()+
-                                                    "\nRESERVA CONFIRMADA");*/
+                                                    "\nRESERVA CONFIRMADA","Nota Fiscal",JOptionPane.INFORMATION_MESSAGE);*/
                         CaixaSaida.append(p.getId()+"\t"+p.getVoo()+"\tR$"+
                                                     voo[j].getPreco()+
                                                     "\tRESERVA CONFIRMADA  \n");
@@ -45,7 +48,7 @@ public class Aeroporto {
                     else{
                        /* JOptionPane.showMessageDialog(null,"Id: "+p.getId()+
                                                      "\nNº do Voo: "+p.getVoo()+
-                                                     "\nVÔO LOTADO");*/
+                                                     "\nVÔO LOTADO","Nota Fiscal",JOptionPane.ERROR_MESSAGE);*/
                         CaixaSaida.append(p.getId()+"\t"+p.getVoo()+"\tR$"+
                                                     voo[j].getPreco()+
                                                     "\tVÕO LOTADO  \n");                            
@@ -56,10 +59,12 @@ public class Aeroporto {
             }
         p.setId(Integer.parseInt(JOptionPane.showInputDialog(null,"Insira Id do passageiro: ")));    
         }  //fecha while
+        
         CaixaSaida.setEditable(false);
+        panel.add(CaixaSaida);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocation(250,250);
-        frame.add(CaixaSaida);
+        frame.add(panel);
         frame.pack();
         frame.setVisible(true);
     }    
